5ff5743a7098188c5be9256e0b593fd1361a76ba
Brian Rosengarten <barosengarten@gmail.com>
2019-01-31 00:13:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,20 +1,20 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path.match(/items/)
- 
+
        = req.path.split("/songs/").last
       song = @@songs.find{|s| s.title == song_title}
- 
+
       resp.write song.artist
     else
       resp.write "Route not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
