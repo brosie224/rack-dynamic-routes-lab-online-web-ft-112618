71c78bdb9fae1b4fb984e323be577275c335063c
Brian Rosengarten <barosengarten@gmail.com>
2019-01-31 00:25:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,9 +8,7 @@ class Application
 
       item_name = req.path.split("/items/").last
 
-      if @@items.include?(item_name)
-        item = @@items.find{|i| i.name == item_name}
-
+      if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else
         resp.write "Item not found"
